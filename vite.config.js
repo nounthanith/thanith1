04961b8238b783f7380fb1ffa1549f1ae8792ecc
nounthanith <nounthanith99@gmail.com>
2025-01-26 08:53:46 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/thanith1/', // This should be at the root level
   plugins: [
-    react(),
-    tailwindcss(),
+    react(), // Plugin for React
+    tailwindcss(), // Plugin for TailwindCSS
   ],
-})
+});
